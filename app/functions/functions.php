41e14b3a4b9445544291eb15e542abd9baa22aab
Bruno Alves <boteistem@gmail.com>
2023-08-07 23:36:55 -0300

    restore: arquivos

--- app/functions/functions.php
+++ app/functions/functions.php
@@ -5,15 +5,3 @@
  * @copyrigh (c) 2023 
  * @license  Open Software License v. 3.0 (OSL-3.0)
  */
-
-function dd($params = [],$die = true){
-    echo '<pre>';
-    print_r($params);
-    echo '</pre>';
-    if ($die) die();
-}
-
-//sanitização dos parametros quando entram no sistema
-function getParams($id){
-   return filter_var($_GET[$id], FILTER_SANITIZE_STRING);
-}
