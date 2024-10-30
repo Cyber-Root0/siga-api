ebc913a31727cf094aa8578c74ba26b36fb595fb
Bruno Alves <boteistem@gmail.com>
2024-10-29 22:46:14 -0300

    fix: linux

--- public/index.php
+++ public/index.php
@@ -6,8 +6,8 @@
  * @license  Open Software License v. 3.0 (OSL-3.0)
  */
 error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
-require_once(__DIR__.'\..\vendor\autoload.php');
-require_once(__DIR__.'\..\app\config\config.php');
-require_once(__DIR__.'\..\app\functions\functions.php');
+require_once(__DIR__.'/../vendor/autoload.php');
+require_once(__DIR__.'/../app/config/config.php');
+require_once(__DIR__.'/../app/functions/functions.php');
 session_start();
 (new \app\core\RouterCore());
